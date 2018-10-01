033a53e8ce53ddf1d0e9e7ff87d7edb2dbf8ab20
Matti Lattu <matti.lattu@ylioppilastutkinto.fi>
2018-10-01 15:48:35 +0300

    Replaced exit button with open meb share (ktp-jako), added missing files

---
+++ src/mebroutines/openmebshare_linux.go
@@ -0,0 +1,27 @@
+package mebroutines
+
+import (
+  "fmt"
+)
+
+func Open_meb_share () {
+  meb_share_path := Get_mebshare_directory()
+
+  Message_debug(fmt.Sprintf("MEB share directory: %s", meb_share_path))
+
+  if ! ExistsDir(meb_share_path) {
+    Message_warning("Cannot open MEB share directory since it does not exist")
+    return
+  }
+
+  run_params := []string{"xdg-open", meb_share_path}
+
+  output,err := Run_get_output(run_params)
+
+  if err != nil {
+    Message_warning("Could not open MEB share directory")
+  }
+
+  Message_debug("MEB share directory open output:")
+  Message_debug(output)
+}
