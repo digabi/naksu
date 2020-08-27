8ab8716fa745b1701e23882800e7bcbe2d4fe9f2
Matti Lattu <matti.lattu@iki.fi>
2020-08-27 08:59:12 +0300

    Download Abitti Etcher image from cloud

--- src/naksu/cloud/cloud.go
+++ src/naksu/cloud/cloud.go
@@ -4,21 +4,74 @@ import (
 	"archive/zip"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 	"path/filepath"
 	"time"
 
-  humanize "github.com/dustin/go-humanize"
+	humanize "github.com/dustin/go-humanize"
 
+	"naksu/constants"
 	"naksu/log"
 	"naksu/mebroutines"
 )
+
+// GetServerImagePath returns path to cached server image (~/ktp/naksu_last_image.zip)
+func GetServerImagePath() string {
+	return filepath.Join(mebroutines.GetVagrantDirectory(), "naksu_last_image.zip")
+}
+
+func downloadServerImage(url string, destinationZipPath string, progressCallbackFn func(string)) error {
+	progressCallbackFn("Contacting server")
+	response, errHttpGet := http.Get(url)
+	if errHttpGet != nil {
+		log.Debug(fmt.Sprintf("HTTP GET from url '%s' gives an error: %v", url, errHttpGet))
+		return errHttpGet
+	}
+	defer response.Body.Close()
+
+	fileSize := uint64(response.ContentLength)
+
+	progressCallbackFn("Opening file")
+	zipFile, errFile := os.OpenFile(destinationZipPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if errFile != nil {
+		log.Debug(fmt.Sprintf("Could not open file '%s' for server image zip: %v", destinationZipPath, errFile))
+		return errFile
+	}
+	defer zipFile.Close()
+
+	progressCallbackFn("Downloading server image")
+
+	var bytesReadTotal uint64
+
+	for {
+		buffer := make([]byte, 100000)
+		bytesRead, errRead := response.Body.Read(buffer)
+		if errRead != nil && errRead != io.EOF {
+			log.Debug(fmt.Sprintf("Error while reading data from '%s': %v", url, errRead))
+			return errRead
+		}
+
+		zipFile.Write(buffer[0:bytesRead])
+
+		bytesReadTotal = bytesReadTotal + uint64(bytesRead)
+		progressCallbackFn(fmt.Sprintf("Downloading image: %d %%", (100*bytesReadTotal)/fileSize))
+
+		if errRead == io.EOF {
+			break
+		}
+	}
+
+	progressCallbackFn("Server image downloaded")
+
+	return nil
+}
 
 // Package "cloud" can be used to query info about box versions available
 // at the cloud (Abitti, yo?) and retrieve these box images. The images
 // can be installed using package "box".
 
-func unZipAbittiImage(zipPath string, destinationImagePath string, progressCallbackFn func(string)) error {
+func unZipServerImage(zipPath string, destinationImagePath string, progressCallbackFn func(string)) error {
 	r, err := zip.OpenReader(zipPath)
 	if err != nil {
 		return fmt.Errorf("could not open zip %s: %v", zipPath, err)
@@ -51,21 +104,21 @@ func unZipAbittiImage(zipPath string, destinationImagePath string, progressCallb
 				unzipDone <- true
 			}()
 
-      waitForZip := true
+			waitForZip := true
 			for waitForZip {
 				time.Sleep(3 * time.Second)
 				imageFileStat, err := fImage.Stat()
 				if err != nil {
 					log.Debug(fmt.Sprintf("Failed to stat raw image file %s: %v", destinationImagePath, err))
 				} else {
-					progressCallbackFn(fmt.Sprintf("Unzipping image: %d %%", uint64(100*imageFileStat.Size()) / file.UncompressedSize64))
+					progressCallbackFn(fmt.Sprintf("Unzipping image: %d %%", uint64(100*imageFileStat.Size())/file.UncompressedSize64))
 				}
 
-        select {
-        case unzipDoneValue := <-unzipDone:
-          waitForZip = ! unzipDoneValue
-        default:
-        }
+				select {
+				case unzipDoneValue := <-unzipDone:
+					waitForZip = !unzipDoneValue
+				default:
+				}
 			}
 
 			progressCallbackFn("unzip finished")
@@ -75,17 +128,24 @@ func unZipAbittiImage(zipPath string, destinationImagePath string, progressCallb
 	return nil
 }
 
-func GetAbittiImage(destinationImagePath string, progressCallbackFn func(string)) error {
-	progressCallbackFn("started")
+func getAndUnzipCloudImage(url string, destinationImagePath string, progressCallbackFn func(string)) error {
+	zipPath := GetServerImagePath()
 
-	err := unZipAbittiImage(filepath.Join(mebroutines.GetHomeDirectory(), "ktp", "ktp-etcher.zip"), destinationImagePath, progressCallbackFn)
-	if err != nil {
-		log.Debug(fmt.Sprintf("Failed to unZipAbittiImage: %v", err))
-		progressCallbackFn("failed")
-		return err
+	errDownload := downloadServerImage(url, zipPath, progressCallbackFn)
+	if errDownload != nil {
+		log.Debug(fmt.Sprintf("Failed to download server image from '%s' to '%s': %v", url, destinationImagePath, errDownload))
+		return errDownload
 	}
 
-	progressCallbackFn("ended")
+	errUnzip := unZipServerImage(zipPath, destinationImagePath, progressCallbackFn)
+	if errUnzip != nil {
+		log.Debug(fmt.Sprintf("Failed to unZipServerImage %s: %v", zipPath, errUnzip))
+		return errUnzip
+	}
 
 	return nil
+}
+
+func GetAbittiImage(destinationImagePath string, progressCallbackFn func(string)) error {
+	return getAndUnzipCloudImage(constants.AbittiEtcherURL, destinationImagePath, progressCallbackFn)
 }
