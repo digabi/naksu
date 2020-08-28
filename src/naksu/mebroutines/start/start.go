48e82ca5c08128f6f232b8a019c01f7e6caba6ac
Matti Lattu <matti.lattu@iki.fi>
2020-08-28 22:50:12 +0300

    Refuse to start server if server is running/not installed

--- src/naksu/mebroutines/start/start.go
+++ src/naksu/mebroutines/start/start.go
@@ -18,6 +18,28 @@ import (
 func Server() {
 	cleanUpTrashVMDirectories()
 
+	isInstalled, errInstalled := box.Installed()
+	if errInstalled != nil {
+		mebroutines.ShowErrorMessage(fmt.Sprintf("Could not start server as we could not detect whether existing VM is installed: %v", errInstalled))
+		return
+	}
+
+	if ! isInstalled {
+		mebroutines.ShowErrorMessage("No server has been installed.")
+		return
+	}
+
+	isRunning, errRunning := box.Running()
+	if errRunning != nil {
+		mebroutines.ShowErrorMessage(fmt.Sprintf("Could not start server as we could not detect whether existing VM is running: %v", errRunning))
+		return
+	}
+
+	if isRunning {
+		mebroutines.ShowErrorMessage("The server is already running.")
+		return
+	}
+
 	err := box.StartCurrentBox()
 	if err != nil {
 		mebroutines.ShowErrorMessage(fmt.Sprintf("Could not start VM: %v", err))
