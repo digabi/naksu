d52a7bd6d27e84c32ef19400a00dbd111a86d9c9
Matti Lattu <matti.lattu@iki.fi>
2020-12-31 17:29:26 +0200

    Open file manager in a goroutine to avoid lags in the UI

--- src/naksu/mebroutines/openmebshare_linux.go
+++ src/naksu/mebroutines/openmebshare_linux.go
@@ -19,19 +19,23 @@ func OpenMebShare() {
 
 	// Try to open MEB share folder with any of these utils
 	// Hopefully we have at least one of them installed!
+	// We do the opening in a goroutine to avoid any lags to the UI
+	
 	openers := [3]string{"xdg-open", "gnome-open", "nautilus"}
 
-	for _, thisOpener := range openers {
-		runParams := []string{thisOpener, mebSharePath}
-		output, err := RunAndGetOutput(runParams, true)
+	go func (){
+		for _, thisOpener := range openers {
+			runParams := []string{thisOpener, mebSharePath}
+			output, err := RunAndGetOutput(runParams, true)
 
-		if err == nil {
-			log.Debug("MEB share directory open output:")
-			log.Debug(output)
+			if err == nil {
+				log.Debug("MEB share directory open output:")
+				log.Debug(output)
 
-			return
+				return
+			}
 		}
-	}
 
-	ShowWarningMessage("Could not open MEB share directory")
+		ShowWarningMessage("Could not open MEB share directory")
+	}()
 }
