3bd21af59050d5fb723880ecabb31625ee258b49
Matti Lattu <matti.lattu@iki.fi>
2021-02-12 16:51:15 +0200

    Fix indentation

--- src/naksu/host/cpucores_darwin.go
+++ src/naksu/host/cpucores_darwin.go
@@ -1,11 +1,11 @@
 package host
 
 import (
-  "naksu/log"
+	"naksu/log"
 )
 
 // GetCPUCoreCount returns number of CPU cores
 func GetCPUCoreCount() (int, error) {
-  log.Debug("At the moment, CPU core count in Darwin is calcuated using runtime.NumCPU() which returns number of CPU threads")
+	log.Debug("At the moment, CPU core count in Darwin is calcuated using runtime.NumCPU() which returns number of CPU threads")
 	return runtime.NumCPU(), nil
 }
